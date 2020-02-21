df89a5917f0c8c3551a612603dfee813d605f900
Lim YoungJin <s2dudwls13@naver.com>
2020-02-21 20:32:33 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '임영진',
+  'birthday': '980224',
+  'gender': '남자',
+  'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '홍길동',
+    'birthday': '000114',
+    'gender': '남자',
+    'job': '프로그래머'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '이순신',
+    'birthday': '931205',
+    'gender': '남자',
+    'job': '디자이너'
+  }
+]
 
 function App() {
   return (
-    <div className="gray-background">
-        <img src={logo} lat="logo"/>
-        <h2>Let's develop management system!</h2>
-    </div> 
+    <div>
+      {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ); }) }
+    </div>
   );
 }
 
